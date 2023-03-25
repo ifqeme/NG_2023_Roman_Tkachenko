ee3e76615995ba5f88d3ef2c16d5a65e81c68c99
ifqeme <romatkachenko3577@gmail.com>
2023-03-25 23:26:59 +0200

    edited Task_4/main.py

--- Lection_1/Task_4/main.py
+++ Lection_1/Task_4/main.py
@@ -7,9 +7,10 @@ discriminant = b**2 - 4*a*c
 if discriminant > 0:
     x1 = (-b + (discriminant)**0.5) / (2*a)
     x2 = (-b - (discriminant)**0.5) / (2*a)
-    print(f"The equation has two roots: {x1} and {x2}")
+    result = f"two roots: {x1} and {x2}"
 elif discriminant == 0:
     x = -b / (2*a)
-    print(f"The equation has one root: {x}")
+    result = f"one root: {x}"
 else:
-    print("The equation has no real roots.")
+    result = f"no real roots"
+print(f"The equation has {result}.")
