b52064d26eead5906f81221e5118c0160c6b86d9
ifqeme <romatkachenko3577@gmail.com>
2023-04-09 01:30:26 +0300

    modified Lection_1/Task_3

--- Lection_1/Task_3/main.py
+++ Lection_1/Task_3/main.py
@@ -2,31 +2,25 @@ first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
 
 operation = input("Input operation: «+, -, *, /, ^, sqrt»: ")
-global result
 
-if operation == "+":
-    result = first_number + second_number
-
-elif operation == "-":
-    result = first_number - second_number
-
-elif operation == "*":
-    result = first_number * second_number
-
-elif operation == "/":
-    result = first_number / second_number
-
-elif operation == "^":
-    result = first_number ** second_number
-
-elif operation == "sqrt":
-    select_number = input("Choose from which number you want to get the root (1 or 2): ")
-    if select_number == "1":
-        result = first_number ** (0.5)
-    if select_number == "2":
-        result = second_number ** (0.5)
-
-else:
-    result = "Error! There is no such operation."
+match operation:
+    case "+":
+        result = first_number + second_number
+    case "-":
+        result = first_number - second_number
+    case "*":
+        result = first_number * second_number
+    case "/":
+        result = first_number / second_number
+    case "^":
+        result = first_number ** second_number
+    case "sqrt":
+        select_number = input("Choose from which number you want to get the root (1 or 2): ")
+        if select_number == "1":
+            result = first_number ** (0.5)
+        elif select_number == "2":
+            result = second_number ** (0.5)
+    case _:
+        result = "NaN"
 
 print(f"Result: {result}")
